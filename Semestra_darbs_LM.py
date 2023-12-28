f5f2d33d57692fead858328303cce1786bdb7574
Tradovid <Ludvigs.mik@gmail.com>
2023-12-28 15:08:23 +0200

    video funkcionalitate gatava

--- Semestra_darbs_LM.py
+++ Semestra_darbs_LM.py
@@ -4,7 +4,9 @@
 #ielikt visu programu zem viena cikla
 #arpus cikla apstradat pilnu atelu
 #zem cikla apstradat tikai atelu kas ir robezas ap bumbu
-#
+#salikt framus atpakal video
+#uz video uzzimet gan masas centra trajektoriju gan visus bumbinas punktus (iztiks bez punktiem)
+#salikt kodu pec iespejas kompaktat iskatas ka visu var izdarit caur video while ciklu
 
 import cv2
 import glob, os
@@ -27,11 +29,6 @@ try:
 except OSError:
     print('Kļūda saglabājot failu')
     
-    
-#while success:
-   # cv2.imwrite(os.path.join(kadri_folder, "frame%d.jpg" % count), frame)
-    #success, frame = vidcap.read() 
-  #  count += 1
     
 def ball_finder(image, rgb_low, rgb_high):
     pixels = []
@@ -60,57 +57,44 @@ def center_finder(top, bot, left, right):
     return center_x,center_y
 
 
-image_path = r'C:\Users\Deloading\Desktop\semestra\Ball_bounce\kadri\frame0.jpg'
-image = cv2.imread(image_path)
-image_test = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 lower_rgb = np.array([90, 105, 10])
 upper_rgb = np.array([255, 195, 70])
-
-points_ball = ball_finder(image_test,lower_rgb,upper_rgb)
-
-
-x, y = zip(*points_ball)
-x = np.array(x)
-y = np.array(y)
-
-left_ball = np.min(x)
-right_ball = np.max(x)
-top_ball = np.min(y)
-bot_ball = np.max(y)
-
-center_x,center_y = center_finder(top_ball,bot_ball,left_ball,right_ball)
-
-print("Top:", top_ball)
-print("Bottom:", bot_ball)
-print("Left:", left_ball)
-print("Right:", right_ball)
-
-
-image_cut = image[top_ball:bot_ball,left_ball:right_ball]
-fig, ax = plt.subplots()
-ax.imshow(cv2.cvtColor(image_cut, cv2.COLOR_BGR2RGB))
-ax.scatter(x,y, color='red', marker='o', s=5)
-ax.scatter(center_x,center_y, color='blue', marker='o', s=5)
-plt.show()
 
 ball_movement_y = []
 ball_movement_x =[]
 time = 0
 
-for frame_number in range(263):
-    image_path = f'C:\\Users\\Deloading\\Desktop\\semestra\\Ball_bounce\\kadri\\frame{frame_number}.jpg'
-    
-    image = cv2.imread(image_path)
-    image_test = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    points_ball = ball_finder_loop(left_ball,right_ball,top_ball,bot_ball,image_test,lower_rgb, upper_rgb)
+output_video_path = os.path.join(script_dir, 'output_video.mp4')
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_video_path, fourcc, fps, (int(vidcap.get(3)), int(vidcap.get(4))))
+
+count = 0
+success, frame = vidcap.read()
+
+image_test = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+points_ball = ball_finder(image_test, lower_rgb, upper_rgb)
+x, y = zip(*points_ball)
+x = np.array(x)
+y = np.array(y)
+left_ball = np.min(x)
+right_ball = np.max(x)
+top_ball = np.min(y)
+bot_ball = np.max(y)
+
+trajectory_points = []
+
+while success:
+    image_test = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    points_ball = ball_finder_loop(left_ball, right_ball, top_ball, bot_ball, image_test, lower_rgb, upper_rgb)
     
     x, y = zip(*points_ball)
     x = np.array(x)
     y = np.array(y)
 
     left_ball = np.min(x)
-    right_ball = np.max(x) 
+    right_ball = np.max(x)
     top_ball = np.min(y)
     bot_ball = np.max(y)
 
@@ -119,14 +103,21 @@ for frame_number in range(263):
     ball_movement_y.append(center_y)
     ball_movement_x.append(center_x)
     time += 1
-    
-    #fig, ax = plt.subplots()
-    #image_cut = image[top_ball:bot_ball,left_ball:right_ball]
-    #ax.imshow(cv2.cvtColor(image_cut, cv2.COLOR_BGR2RGB))
-    #ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    #ax.scatter(x,y, color='red', marker='o', s=5)
-    #ax.scatter(center_x,center_y, color='blue', marker='o', s=5)
-    #plt.show()
+
+    cv2.circle(frame, (int(center_x), int(center_y)), 5, (0, 0, 255), -1)
+    trajectory_points.append((int(center_x), int(center_y)))
+
+    for i in range(1, len(trajectory_points)):
+        cv2.line(frame, trajectory_points[i - 1], trajectory_points[i], (255, 0, 0), 2)
+
+    out.write(frame)
+
+    success, frame = vidcap.read()
+    count += 1
+
+out.release()
+
+cv2.destroyAllWindows()
 
 fig, ax = plt.subplots()
 ax.plot(np.linspace(0, time-1, time), ball_movement_y)
